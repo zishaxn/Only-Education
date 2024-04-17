bd9411efd03956cc39767e750674a96ad3172829
zishan chaudhary <zishan8291@gmail.com>
2024-04-17 16:23:37 +0530

    initial-2

--- client/src/API/ApiRoutes.js
+++ client/src/API/ApiRoutes.js
@@ -1,8 +1,7 @@
-const host = "http://localhost:5400";
+const host = "https://only-education-server.onrender.com";
 
 export const login = `${host}/api/login`;
 export const getquestions = `${host}/api/questions`;
 export const saveResult = `${host}/api/saveresult`;
 export const getSubmissions = `${host}/api/getsubmissions`;
 export const getanswers = `${host}/api/getuseranswers`;
-
